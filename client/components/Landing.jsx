021edaae31b65547e14ccaffe21e25d35020d6cc
Emily Parkes <emilycoco@me.com>
2018-02-02 15:04:59 +1300

    changed font on landing page

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -13,6 +13,7 @@ class Landing extends React.Component {
       <div className='landing'>
         <h2>How to Play:</h2>
         <h4>Click the bombs as fast as you can before time runs out to defuse the bomb!</h4>
+        {/* <h4></h4>  */}
         <button className='start' onClick={this.props.start}>START</button>
       </div>
     )
